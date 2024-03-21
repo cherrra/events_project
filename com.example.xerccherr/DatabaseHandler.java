012b770c8b236842229bcbb6e2e85ff81d99c2b2
cherrra <94289157+cherrra@users.noreply.github.com>
2024-03-21 08:13:51 +0300

    Create DatabaseHandler.java

---
+++ com.example.xerccherr/DatabaseHandler.java
@@ -0,0 +1,59 @@
+package com.example.xerccherr;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class DatabaseHandler extends Configs {
+    Connection dbConnection;
+    public Connection getDbConnection()
+            throws ClassNotFoundException, SQLException {
+        String connectionString = "jdbc:mysql://" + dbHost + ":"
+                + dbPort + "/" + dbName;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        dbConnection = DriverManager.getConnection(connectionString,
+                dbUser, dbPass);
+        return dbConnection;
+    }
+
+    public void signUpUser(User user){
+        String insert = "INSERT INTO " + Const.USER_TABLE + "(" +
+                Const.USERS_NAME + "," + Const.USERS_LOGIN + "," +
+                Const.USERS_PASSWORD + ")" +
+                "VALUES(?, ?, ?)";
+
+        try {
+            PreparedStatement prSt = getDbConnection().prepareStatement(insert);
+            prSt.setString(1, user.getName());
+            prSt.setString(2, user.getLogin());
+            prSt.setString(3, user.getPassword());
+            prSt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ResultSet getUser(User user) {
+        ResultSet resSet = null;
+
+        String select = "SELECT * FROM " + Const.USER_TABLE + " WHERE " +
+                Const.USERS_NAME + "=? AND " + Const.USERS_PASSWORD + "=?";
+        try {
+            PreparedStatement prSt = getDbConnection().prepareStatement(select);
+            prSt.setString(1, user.getName());
+            prSt.setString(2, user.getPassword());
+
+            resSet = prSt.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return resSet;
+    }
+
+}
