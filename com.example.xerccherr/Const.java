31a78157bd7ddff0249f30986f9a5294b9955580
cherrra <94289157+cherrra@users.noreply.github.com>
2024-03-21 08:12:35 +0300

    Create Const.java

---
+++ com.example.xerccherr/Const.java
@@ -0,0 +1,11 @@
+package com.example.xerccherr;
+
+public class Const {
+    public static final String USER_TABLE = "customers"; //первая таблица
+    public static final String USERS_ID = "id_cust";
+    public static final String USERS_NAME = "name";
+    public static final String USERS_LOGIN = "login";
+    public static final String USERS_PASSWORD = "password";
+
+
+}
