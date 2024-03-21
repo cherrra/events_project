e536ae5938ebaa88a8c20b5dc7b53ee67c886d1f
cherrra <94289157+cherrra@users.noreply.github.com>
2024-03-21 08:15:54 +0300

    Create PolzovatelController.java

---
+++ com.example.xerccherr/PolzovatelController.java
@@ -0,0 +1,50 @@
+package com.example.xerccherr;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class PolzovatelController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button glavnayButton;
+
+    @FXML
+    void initialize() {
+        glavnayButton.setOnAction(actionEvent -> {
+            //прячем первое окно
+            glavnayButton.getScene().getWindow().hide();
+            //отображаем нужное окно
+            FXMLLoader loader = new FXMLLoader();
+            //указываем место расположения нужного файла
+            loader.setLocation(getClass().getResource("hello-view.fxml"));
+            //загружаем файл для дальнейшего отображения
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        });
+
+
+    }
+
+
+}
