575c5b2370dd5e0557e8a0971251adb3515aee46
cherrra <94289157+cherrra@users.noreply.github.com>
2024-03-21 08:12:14 +0300

    Create Configs.java

---
+++ com.example.xerccherr/Configs.java
@@ -0,0 +1,10 @@
+package com.example.xerccherr;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "root";
+    protected String dbName = "concert";
+
+}
